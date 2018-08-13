f387bbc1182d72ba7052e24c0b6d6d62d271ba93
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-08-13 09:18:05 -0400

    Add support to screenshot editor
    Change-Id: Id1289b70863673b3a6ccd3bf0aa97e68437ee187

--- config.mk
+++ config.mk
@@ -18,7 +18,8 @@ PRODUCT_PACKAGES += \
     WallpaperPickerGooglePrebuilt \
     NexusLauncherPrebuilt \
     TimeZoneDataPrebuilt \
-    WellbeingPrebuilt
+    WellbeingPrebuilt \
+    MarkupGoogle
 
 TARGET_MINIMAL_APPS ?= false
 
@@ -61,6 +62,11 @@ PRODUCT_COPY_FILES += \
 # Set Pixel blue light theme on Gboard
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.ime.theme_id=5
+
+# Markup libs
+PRODUCT_COPY_FILES += \
+    vendor/pixelstyle/lib/libsketchology_native.so:system/lib/libsketchology_native.so \
+    vendor/pixelstyle/lib64/libsketchology_native.so:system/lib64/libsketchology_native.so
 
 # Include package overlays
 DEVICE_PACKAGE_OVERLAYS += \
