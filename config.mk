01291d6988290857e4fb9a33beed46af30c8825a
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-08-15 09:58:28 -0400

    Merge sysconfig in one file
    Change-Id: If61ce44b001726b69c8cfa67e87bcfbfce35107b

--- config.mk
+++ config.mk
@@ -55,9 +55,7 @@ PRODUCT_COPY_FILES += \
 
 # Pixel sysconfig
 PRODUCT_COPY_FILES += \
-    vendor/pixelstyle/etc/sysconfig/nexus.xml:system/etc/sysconfig/nexus.xml \
-    vendor/pixelstyle/etc/sysconfig/pixel_2017_exclusive.xml:system/etc/sysconfig/pixel_2017_exclusive.xml \
-    vendor/pixelstyle/etc/sysconfig/pixel_experience_2017.xml:system/etc/sysconfig/pixel_experience_2017.xml
+    vendor/pixelstyle/etc/sysconfig/pixel.xml:system/etc/sysconfig/pixel.xml
 
 # Set Pixel blue light theme on Gboard
 PRODUCT_PROPERTY_OVERRIDES += \
