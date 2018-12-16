PixelExperience/vendor_pixelstyle

Weather: Add default permissions
Change-Id: Icba08d4b98452b9d57c771e1719fb002c7019cf5

config.mk
@@ -84,4 +84,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     org.pixelexperience.weather.revision=2
 
 PRODUCT_COPY_FILES += \
-    vendor/pixelstyle/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml
+    vendor/pixelstyle/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
+    vendor/pixelstyle/etc/default-permissions/org.pixelexperience.weather.client.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml

vansh123456/android_vendor_revengeui

config.mk
@@ -77,4 +77,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     org.pixelexperience.weather.revision=2
 
 PRODUCT_COPY_FILES += \
-    vendor/revengeui/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml
+    vendor/revengeui/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
+    vendor/revengeui/etc/default-permissions/org.pixelexperience.weather.client.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml